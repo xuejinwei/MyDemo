f64627d8e11556f0b1f58c3bc387a2393a348b16
xuejinwei <xuejinwei@outlook.com>
2016-01-14 23:25:58 +0800

    扩展百分比类库， DisplayMetrics

--- app/src/main/java/com/example/designsupportlibraryuseing/MainActivity.java
+++ app/src/main/java/com/example/designsupportlibraryuseing/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -30,7 +32,7 @@ public class MainActivity extends BaseActivity {
 
     private DrawerLayout mDrawerLayout;
     private Toolbar toolbar;
-
+    private String TAG = "Screen_size";
 
     @Subscribe
     public void onToolbarChange(ToolbarChangeEvent event) {
@@ -50,6 +52,19 @@ public class MainActivity extends BaseActivity {
             ab.setHomeAsUpIndicator(R.drawable.ic_menu);
             ab.setDisplayHomeAsUpEnabled(true);
         }
+
+        // 获取屏幕密度(在Context环境下，比如在Activity中执行这些代码或者传入Context对象去获得DisplayMetrics）
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        //getWindowManager().getDefaultDisplay().getMetrics(dm);
+        float density = dm.density;        // 屏幕密度（像素比例：0.75/1.0/1.5/2.0,3.0,4等）
+        int densityDPI = dm.densityDpi;        // 屏幕密度(这里是系统密度）（每寸像素：120/160/240/320,480,640等）
+        float xdpi = dm.xdpi; //这两个是水平和垂直方向上的dpi，如果要计算水平和垂直方向上的实际dp值，可以通过后面`屏幕的宽高差异`中的内容计算。不过你倒是可以通过这两个值大致计算屏幕的宽和高的英寸
+        float ydpi = dm.ydpi;
+        Log.i(TAG, "  DisplayMetrics: " + "xdpi=" + xdpi + "; ydpi=" + ydpi);
+        Log.i(TAG, "  DisplayMetrics: " + "density=" + density + "; densityDPI=" + densityDPI);
+        int screenWidth = dm.widthPixels;        // 屏幕像素宽（像素，如：480px）
+        int screenHeight = dm.heightPixels;        // 屏幕像素高（像素，如：800px）
+        Log.i(TAG, "  DisplayMetrics" + "screenWidth=" + screenWidth + "; screenHeight=" + screenHeight);
 
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
